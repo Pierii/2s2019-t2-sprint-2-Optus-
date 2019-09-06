6686828b532f7abc728c87bd15522115ccbf7e87
Rafael Pieri Almeida <46181942866@info.local>
2019-09-06 08:51:40 -0300

    06.09.2019T08:50

---
+++ Senai.Optus.WebApi/Repositories/EstiloRepository.cs
@@ -0,0 +1,49 @@
+﻿using Senai.Optus.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Optus.WebApi.Repositories
+{
+    public class EstiloRepository
+    {
+        public List<Estilos> Listar()
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                return ctx.Estilos.ToList();
+            }
+        }
+
+        public void Cadastrar(Estilos estilo)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                ctx.Estilos.Add(estilo);
+                ctx.SaveChanges();
+            }
+        }
+
+        public Estilos BuscarPorId(int id)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                return ctx.Estilos.FirstOrDefault(x => x.IdEstilo == id);
+            }
+        }
+
+        public void Atualizar(Estilos estilos)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                Estilos CategoriaBuscada = ctx.Estilos.FirstOrDefault(x => x.IdEstilo == estilos.IdEstilo);
+                CategoriaBuscada.Nome = estilos.Nome;
+                ctx.Estilos.Update(CategoriaBuscada);
+                ctx.SaveChanges();
+            }
+        }
+
+
+    }
+}
---
+++ Senai.Optus.WebApi/Repositories/UsuarioRepository.cs
@@ -0,0 +1,23 @@
+﻿using Senai.Optus.WebApi.Domains;
+using Senai.Optus.WebApi.ViewModels;
+using System.Linq;
+
+namespace Senai.Optus.WebApi.Controllers
+{
+    public class UsuarioRepository
+    {
+        public Usuarios BuscarPorEmailESenha(LoginViewModel login)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                Usuarios UsuarioBuscado = ctx.Usuarios.FirstOrDefault(x => x.Email == login.Email && x.Senha == login.Senha);
+
+                if (UsuarioBuscado == null)
+                {
+                    return null;
+                }
+                return UsuarioBuscado;
+            }
+        }
+    }
+}
